4fd998c455b756f01c98dc1ed08929a7f0ba270e
VinayThamke <vinspace.89@gmail.com>
2026-03-05 22:42:39 +0530

    removed comment

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,6 @@ const app = express();
 const PORT = 4000;
 
 // 1. Express Middleware
-// app.use(cors({
-//   origin: ["http://localhost:5173", "http://localhost:5001"],
-//   credentials: true
-// }));
 app.use(express.json());
 
 // 2. Mock Database
